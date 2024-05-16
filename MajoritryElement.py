7c2f037b0cc1f1051ab50104061cefd2b63e0514
Binyamin <Binyamin.cohen111@gmail.com>
2024-05-17 01:06:04 +0300

    Final version

---
+++ MajoritryElement.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+
+def majority_element(num_list):
+    max_value_item = max(Counter(num_list).values())
+    return [key for key, val in Counter(num_list).most_common() if val == max_value_item]
+
+
+print(majority_element([3, 4, 3, 4]))
